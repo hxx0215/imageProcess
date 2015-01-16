6526d2bd282e7fcc31de8aa0ada2fbd4fe44b14b
hxx0215 <hxx0215@126.com>
2015-01-16 14:08:29 +0800

    项目初始化

---
+++ ImageUtils/ImageUtils/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageUtils
+//
+//  Created by hxx on 1/16/15.
+//  Copyright (c) 2015 hxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageUtils/ImageUtils/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageUtils
+//
+//  Created by hxx on 1/16/15.
+//  Copyright (c) 2015 hxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
